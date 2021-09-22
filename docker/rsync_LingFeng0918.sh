8da6974664e4774bd037e3754880b54b824d2085
Sirius2016 <515104645@qq.com>
2021-09-22 15:44:20 +0800

    chmod

--- docker/rsync_LingFeng0918.sh
+++ docker/rsync_LingFeng0918.sh
@@ -3,6 +3,6 @@ git clone https://github.com.cnpmjs.org/LingFeng0918/jd_scripts.git
 cd jd_scripts
 ls |grep -v "md"|xargs -I {} cp -rf {} ../
 cp docker/crontab_list.sh /scripts/docker/crontab_list.sh
-sed -i '/短期活动/a\15 0 * * * git clone https://github.com.cnpmjs.org/Sirius2016/jd_scripts-fork.git  /scripts' /scripts/docker/crontab_list.sh
+sed -i '/短期活动/a\0 */8 * * * sh /scripts/docker/rsync_LingFeng0918.sh' /scripts/docker/crontab_list.sh
 cd ../
 rm -rf jd_scripts
