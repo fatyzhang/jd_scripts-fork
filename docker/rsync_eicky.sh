f074423c2a14b6ea4fe35145d4dfb006a1c82afb
Sirius2016 <Sirius2016@users.noreply.github.com>
2021-12-08 11:01:55 +0800

    update

--- docker/rsync_eicky.sh
+++ docker/rsync_eicky.sh
@@ -2,12 +2,12 @@
 cd /scripts/
 git clone https://github.com.cnpmjs.org/eicky/jd_scripts.git
 cd jd_scripts
-ls |egrep -v "md|Loon|docker|myCrontab|QuantumultX|Surge"|xargs -I {} cp -rf {} ../
+ls |egrep -v "md|Loon|docker|myCrontab|QuantumultX|Surge|clean_car"|xargs -I {} cp -rf {} ../
 cp -rf docker/crontab_list.sh /scripts/docker/crontab_list.sh
 sed -i '/jd_cleancart/d' /scripts/docker/crontab_list.sh
 sed -i '/LingFeng0918/d' /scripts/docker/crontab_list.sh
 sed -i '/#活动#/a\0 */4 * * * sh /scripts/docker/rsync_eicky.sh' /scripts/docker/crontab_list.sh
 sed -i '4i 0 */4 * * * sh /scripts/docker/rsync_eicky.sh' /scripts/docker/crontab_list.sh
 cd ../
-ls|egrep "jd_cleancart|jddj|jd_read|joy"|xargs -I {} rm -rf {}
+ls|egrep "jd_cleancart|jddj|jd_read|joy|jd_clean"|xargs -I {} rm -rf {}
 rm -rf jd_scripts
